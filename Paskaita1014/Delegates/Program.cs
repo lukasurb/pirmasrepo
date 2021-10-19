bf7e163a5d5984a29bb525c377921152367f954e
lukasurb <lukasurb@gmail.com>
2021-10-19 21:28:26 +0300

    Delegatai

---
+++ Paskaita1014/Delegates/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Delegates
+{
+    public delegate int DoOperation (int m1, int m2);
+
+    class Program
+    {
+        private static DoOperation darytiVeiksma;
+
+        static void Main(string[] args)
+        {
+            darytiVeiksma += MyMultiply;
+            darytiVeiksma += MySum;
+            darytiVeiksma += MySum;
+            darytiVeiksma += MyMultiply;
+
+            darytiVeiksma(16, 36);
+
+
+
+            Console.ReadLine();
+        }
+
+        static int MyMultiply(int m1, int m2)
+        {
+            int sandauga = m1 * m2;
+            Console.WriteLine(sandauga);
+            return sandauga;
+        }
+
+        static int MySum(int m1, int m2)
+        {
+            int suma = m1 + m2;
+            Console.WriteLine(suma);
+            return suma;
+        }
+    }
+}
